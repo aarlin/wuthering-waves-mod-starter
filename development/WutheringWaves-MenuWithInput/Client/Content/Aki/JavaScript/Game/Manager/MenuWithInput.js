c138b289397d21bb6788e7e0d2d7f63ba0c7ff22
Aaron Lin <aaronlin256@gmail.com>
2024-06-12 01:56:30 -0400

    WIP simple menu display on hotkey

---
+++ development/WutheringWaves-MenuWithInput/Client/Content/Aki/JavaScript/Game/Manager/MenuWithInput.js
@@ -0,0 +1,78 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: !0 }),
+  (exports.MenuWithInput = void 0);
+const UiControllerBase_1 = require("../../Ui/Base/UiControllerBase"),
+  UiManager_1 = require("../../Ui/UiManager"),
+  InputSettings_1 = require("../InputSettings/InputSettings"),
+  InputController_1 = require("../Input/InputController"),
+  ModelManager_1 = require("./ModelManager"),
+  ScrollingTipsController_1 = require("../Module/ScrollingTips/ScrollingTipsController");
+
+class MenuWithInput extends UiControllerBase_1.UiControllerBase {
+  static Settings = {
+    CurrentString: -1,
+  };
+
+  static StartMod() {
+    InputSettings_1.InputSettings.AddActionMapping("ShowMenuWithInput", "F12");
+  }
+
+  static HandleKeyInputs(keyPressed) {
+    ScrollingTipsController_1.ScrollingTipsController.ShowTipsByText(keyPressed)
+    if (InputController_1.InputController.IsCustomKeyUp("F12")) {
+      ScrollingTipsController_1.ScrollingTipsController.ShowTipsByText("F12 key was pressed")
+      this.showMenuWithInput();
+    }
+  }
+
+  static listenKey(description, key) {
+    return InputController_1.InputController.IsCustomKeyUp(key);
+  }
+
+  static showMenuWithInput() {
+    // UiManager_1.UiManager.OpenView("CdKeyInputView", {
+    //   Title: "Menu With Input",
+    //   CustomFunc: async (string) => {
+    //     this.Settings.CurrentString = string;
+    //     ScrollingTipsController_1.ScrollingTipsController.ShowTipsByText("Showing menu with input");
+    //     UiManager_1.UiManager.CloseView("UidView");
+    //     UiManager_1.UiManager.OpenView("UidView");
+    //   },
+    //   InputText: this.Settings.CurrentString,
+    //   DefaultText: "CurrentString",
+    //   IsCheckNone: true,
+    //   NeedFunctionButton: false,
+    // });
+
+    UiManager_1.UiManager.OpenView("CommonSingleInputView", {
+      Title: "Menu With Input",
+      CustomFunc: async (string) => {
+        this.Settings.CurrentString = string;
+        ScrollingTipsController_1.ScrollingTipsController.ShowTipsByText("Showing menu with input");
+        UiManager_1.UiManager.CloseView("UidView");
+        UiManager_1.UiManager.OpenView("UidView");
+      },
+      InputText: this.Settings.CurrentString,
+        DefaultText: "CurrentString",
+        IsCheckNone: true,
+        NeedFunctionButton: false,
+    });
+
+    // var e = {
+    // 	TitleTextArgs: new LguiUtil_1.TableTextArgNew(
+    // 		"PrefabTextItem_CDKey_Title",
+    // 	),
+    // 	ConfirmFunc: CdKeyInputController_1.CdKeyInputController.RequestCdKey,
+    // 	DefaultText:
+    // 		MultiTextLang_1.configMultiTextLang.GetLocalTextNew(
+    // 			"CDKey_InputEmpty",
+    // 		) ?? "",
+    // 	InputText: "",
+    // 	IsCheckNone: !0,
+    // 	NeedFunctionButton: !0,
+    // };
+    // UiManager_1.UiManager.OpenView("CdKeyInputView", e);
+  }
+
+}
+exports.MenuWithInput = MenuWithInput;
---
+++ development/WutheringWaves-MenuWithInput/Client/Content/Aki/JavaScript/Game/Module/UidShow/UidView.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: !0 }),
+  (exports.UidView = void 0);
+const UE = require("ue"),
+  ModelManager_1 = require("../../Manager/ModelManager"),
+  UiViewBase_1 = require("../../Ui/Base/UiViewBase"),
+  MenuWithInput_1 = require("../../Manager/MenuWithInput"),
+  LguiUtil_1 = require("../Util/LguiUtil");
+class UidView extends UiViewBase_1.UiViewBase {
+  OnRegisterComponent() {
+    this.ComponentRegisterInfos = [[0, UE.UIText]];
+  }
+  OnStart() {
+    MenuWithInput_1.MenuWithInput.StartMod();
+
+    LguiUtil_1.LguiUtil.SetLocalText(
+      this.GetText(0),
+      "FriendMyUid",
+      ModelManager_1.ModelManager.FunctionModel.PlayerId.toString(),
+    );
+  }
+}
+exports.UidView = UidView;
+//# sourceMappingURL=UidView.js.map
